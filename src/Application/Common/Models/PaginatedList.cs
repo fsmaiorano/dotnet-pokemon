77e7b6a930dddcdaa506dc2719dfa3d9a1f9b269
Fábio <fsmaiorano@gmail.com>
2023-10-25 16:49:44 +0100

    add links to next and previous pagination

--- src/Application/Common/Models/PaginatedList.cs
+++ src/Application/Common/Models/PaginatedList.cs
@@ -2,21 +2,24 @@
 
 namespace Application.Common.Models;
 
-public class PaginatedList<T>
+public record PaginatedList<T>
 {
     public IReadOnlyCollection<T> Items { get; }
     public int PageNumber { get; }
     public int TotalPages { get; }
     public int TotalCount { get; }
+    public string? PreviousPage { get; set; }
+    public string? NextPage { get; set; }
 
-    public PaginatedList(IReadOnlyCollection<T> items, int count, int pageNumber, int pageSize)
+    public PaginatedList(IReadOnlyCollection<T> items, int count, int pageNumber, int pageSize, string? previousPage, string? nextPage)
     {
         PageNumber = pageNumber;
         TotalPages = (int)Math.Ceiling(count / (double)pageSize);
         TotalCount = count;
         Items = items;
+        PreviousPage = previousPage;
+        NextPage = nextPage;
     }
-
     public bool HasPreviousPage => PageNumber > 1;
 
     public bool HasNextPage => PageNumber < TotalPages;
@@ -25,7 +28,11 @@ public class PaginatedList<T>
     {
         var count = await source.CountAsync();
         var items = await source.Skip((pageNumber - 1) * pageSize).Take(pageSize).ToListAsync();
+        var urls = (Environment.GetEnvironmentVariable("ASPNETCORE_URLS")!.Split(";") ?? Array.Empty<string>()).FirstOrDefault();
 
-        return new PaginatedList<T>(items, count, pageNumber, pageSize);
+        var previousPage = pageNumber > 1 ? $"{urls}/pokemon?pageNumber={pageNumber - 1}&pageSize={pageSize}" : null;
+        var nextPage = pageNumber < count ? $"{urls}/pokemon?pageNumber={pageNumber + 1}&pageSize={pageSize}" : null;
+
+        return new PaginatedList<T>(items, count, pageNumber, pageSize, previousPage, nextPage);
     }
 }
