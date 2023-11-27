9e0c06a0e8d5b950ced1c0fd0fce956bb8bf736c
Fábio Maiorano <fsmaiorano@gmail.com>
2023-11-27 17:25:35 +0000

    wip

--- src/App/Services/PokemonService.cs
+++ src/App/Services/PokemonService.cs
@@ -17,6 +17,7 @@ namespace App.Services
 
         public async Task<GenericResponse<Pokemon>> GetPokemon(int pageNumber, int pageSize)
         {
+            //var response = await HttpHelper.GetAsync<GenericResponse<Pokemon>>($"http://10.0.2.2:5268/pokemonWithPagination?pageNumber={pageNumber}&pageSize={pageSize}");
             var response = await HttpHelper.GetAsync<GenericResponse<Pokemon>>($"http://localhost:5268/pokemonWithPagination?pageNumber={pageNumber}&pageSize={pageSize}");
             return response;
         }
